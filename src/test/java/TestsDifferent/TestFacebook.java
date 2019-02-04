31e3e5854a62885e7216dd3a2dcc933598f07296
Зарина Божик <zarina.bozhik@gmail.com>
2019-02-04 13:16:58 +0200

    added Error message verifying

--- src/test/java/TestsDifferent/TestFacebook.java
+++ src/test/java/TestsDifferent/TestFacebook.java
@@ -1,5 +1,6 @@
 package TestsDifferent;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -13,31 +14,24 @@ public class TestFacebook {
     public void testFaceBook() {
         System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
-
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://facebook.com/");
         WebElement login = driver.findElement(By.xpath("//input[@id=\"email\"]"));
-
-        login.sendKeys("11111");
-
-
+        login.sendKeys("55555545");
         WebElement password = driver.findElement(By.xpath("//input[@id=\"pass\"]"));
-        password.sendKeys("1111");
-
+        password.sendKeys("1555111");
         password.submit();
         WebElement message = driver.findElement(By.xpath("//div[@class=\"_4rbf _53ij\"]"));
-        if (message.isDisplayed()) {
-            WebElement recoverPass = driver.findElement(By.id("pass"));
-            recoverPass.sendKeys("lololo");
-            WebElement loginButton = driver.findElement(By.id("loginbutton"));
-            loginButton.click();
-
-        }
-
-
+        String actual_error = message.getText();
+        System.out.println(actual_error);
+        String expected_error = "The email or phone number you’ve entered doesn’t match any account. Sign up for an account.";
+        if(actual_error.contains(expected_error)){
+            System.out.println("Test Case Passed");
+        }else
+            System.out.println("Test Case Passed");
+        driver.quit();
     }
-
 }
 
 
