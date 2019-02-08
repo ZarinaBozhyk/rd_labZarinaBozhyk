eb3c323796e647c4f010c9ece5e230caa7a85798
Oleksandr Antonkin <oleksandr.antonkin@centire.com>
2019-02-08 16:23:39 +0200

    - code review

--- src/main/java/company/Lesson12/GooglePage.java
+++ src/main/java/company/Lesson12/GooglePage.java
@@ -10,7 +10,8 @@ public class GooglePage {
         this.driver = driver;
     }
 
-    public String checkURL(){
+    // TODO: name convention. This methods returns URL, so better to call it getURL(). You are not making any checks/verifications here.
+    public String checkURL() {
         String url = driver.getCurrentUrl();
         return url;
     }
